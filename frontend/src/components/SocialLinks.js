297b2f01529c8bdd0da43772d0cd5954325fb883
Olga <o.nikolaichenko@ya.ru>
2024-08-16 14:02:27 +0200

    EmailForm component added, SocialLinks styled

--- frontend/src/components/SocialLinks.js
+++ frontend/src/components/SocialLinks.js
@@ -5,17 +5,24 @@ import styles from './SocialLinks.module.css';
 
 const SocialLinks = () => {
   return (
-    <div className={`${styles.socialLinksWrap} container`}>
-      <a href="https://vk.com" target="_blank" rel="noopener noreferrer">
-        <RiVkLine />
-      </a>
-      <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
-        <RiYoutubeLine />
-      </a>
-      <a href="https://telegram.org/" target="_blank" rel="noopener noreferrer">
-        <RiTelegramLine />
-      </a>
-    </div>
+    <section className={`${styles.socialLinksWrap} container mb`}>
+      <h2 className={`grechen-fuemen-regular sectionTitle`}>Our socials</h2>
+      <div>
+        <a href="https://vk.com" target="_blank" rel="noopener noreferrer">
+          <RiVkLine />
+        </a>
+        <a href="https://youtube.com" target="_blank" rel="noopener noreferrer">
+          <RiYoutubeLine />
+        </a>
+        <a
+          href="https://telegram.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <RiTelegramLine />
+        </a>
+      </div>
+    </section>
   );
 };
 
