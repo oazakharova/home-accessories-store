56d351e1c02b66072a6c392e6601e670bbb7e675
Olga <o.nikolaichenko@ya.ru>
2024-08-21 17:26:41 +0200

    NotFound component has been added

--- frontend/src/pages/NotFound.js
+++ frontend/src/pages/NotFound.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './NotFound.module.css';
+import notFoundGif from '../assets/images/errorGif.gif';
 
 const NotFound = () => {
-  return <div className={styles.notFoundWrap}>NotFound</div>;
+  return (
+    <div className={`${styles.notFoundWrap} container`}>
+      <h1 className={`${styles.notFoundTitle} mb`}>
+        Sorry, we can't find this page or something has gone wrong...
+      </h1>
+      <img className={`${styles.notFoundImg}`} src={notFoundGif} alt=""></img>
+      {/* <nav>
+        <Link to="/">Home</Link>
+        {' | '}
+        <Link to="/products">Products</Link>
+        {' | '}
+        <Link to="/about">About</Link>
+      </nav> */}
+      <h2>
+        <Link to="/">Go back</Link>
+      </h2>
+    </div>
+  );
 };
 
 export default NotFound;
