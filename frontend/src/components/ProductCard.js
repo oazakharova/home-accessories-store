465afe5c0105ef21af360b54ebe9c38c7c0111be
Olga <o.nikolaichenko@ya.ru>
2024-08-21 16:18:18 +0200

    temporary "Go to cart" link after adding item to cart has been added

--- frontend/src/components/ProductCard.js
+++ frontend/src/components/ProductCard.js
@@ -1,10 +1,20 @@
-import React from 'react';
-import { useCart } from '../context/CartContext';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import styles from './ProductCard.module.css';
+import { useCart } from '../context/CartContext';
 
 const ProductCard = ({ product }) => {
   const { addToCart } = useCart();
+  const [showGoToCart, setShowGoToCart] = useState(false);
+
+  const handleAddToCart = () => {
+    addToCart(product);
+    setShowGoToCart(true);
+    setTimeout(() => {
+      setShowGoToCart(false);
+    }, 4000);
+  };
 
   return (
     <li className={styles.productCard}>
@@ -15,12 +25,18 @@ const ProductCard = ({ product }) => {
       />
       <h5 className={styles.productName}>{product.name}</h5>
       <h6 className={styles.productPrice}>Price: ${product.price}</h6>
-      <button
-        onClick={() => addToCart(product)}
-        className={styles.productButton}
-      >
+      {/* <button onClick={handleAddToCart} className={styles.productButton}>
         Add to card
-      </button>
+      </button> */}
+      {showGoToCart ? (
+        <button className={styles.productButton}>
+          <Link to="/cart">Go to cart </Link>
+        </button>
+      ) : (
+        <button onClick={handleAddToCart} className={styles.productButton}>
+          Add to cart
+        </button>
+      )}
       {product.isNew && <div className={styles.isNew}>NEW</div>}
     </li>
   );
